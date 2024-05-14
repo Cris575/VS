535b8af9608b314d9e4b24816702f05cddbff16a
Cris <73599015+Cris575@users.noreply.github.com>
2024-05-14 13:52:34 -0600

    Update SQLQuery13.sql

--- VS/SQL/SQLQuery13.sql
+++ VS/SQL/SQLQuery13.sql
@@ -5,131 +5,127 @@ FROM [CLHOURS].[dbo].[CLHOURS]
 WHERE [ProdDate] BETWEEN DATEADD(DAY,1,'2024-03-11 00:00:00') AND DATEADD(DAY,1,'2024-04-21 00:00:00')
 GROUP BY [ProdDate];
 
-
-
-
+-----------------------------------
 
 SELECT 
-    DATEPART(week, [ProdDate]) AS semana,
-    FORMAT(ROUND(SUM([Standard]) / 2040 * 100, 2), '0.##') AS [Standard]
+DATEPART(week, [ProdDate]) AS semana,
+FORMAT(ROUND(SUM([Standard]) / 2040 * 100, 2), '0.##') AS [Standard]
 FROM 
-    [CLHOURS].[dbo].[CLHOURS]
+[CLHOURS].[dbo].[CLHOURS]
 WHERE 
-    [ProdDate] >= '2023-09-25 00:00:00' AND [ProdDate] <= '2024-04-21 00:00:00' 
-    AND DATEPART(weekday, [ProdDate]) >= 2 AND DATEPART(weekday, [ProdDate]) <= 8 
+[ProdDate] >= '2023-09-25 00:00:00' AND [ProdDate] <= '2024-04-21 00:00:00' 
+AND DATEPART(weekday, [ProdDate]) >= 2 AND DATEPART(weekday, [ProdDate]) <= 8 
 GROUP BY 
-    DATEPART(week, [ProdDate])
+DATEPART(week, [ProdDate])
 ORDER BY 
-    semana;
+semana;
 
-
-	
-SELECT FORMAT(ROUND(SUM([Standard]) / 2040 * 100, 2), '0.##') AS [Standard]
-FROM 
-    [CLHOURS].[dbo].[CLHOURS]
-	WHERE [ProdDate] > '2023-09-24 00:00:00' AND [ProdDate] < '2023-10-02 00:00:00'
+-----------------------------------
 
 SELECT FORMAT(ROUND(SUM([Standard]) / 2040 * 100, 2), '0.##') AS [Standard]
 FROM 
-    [CLHOURS].[dbo].[CLHOURS]
-	WHERE [ProdDate] > '2023-10-01 00:00:00' AND [ProdDate] < '2023-10-9 00:00:00'
-
+[CLHOURS].[dbo].[CLHOURS]
+WHERE [ProdDate] > '2023-09-24 00:00:00' AND [ProdDate] < '2023-10-02 00:00:00'
 
 SELECT FORMAT(ROUND(SUM([Standard]) / 2040 * 100, 2), '0.##') AS [Standard]
 FROM 
-    [CLHOURS].[dbo].[CLHOURS]
-	WHERE [ProdDate] > '2023-10-08 00:00:00' AND [ProdDate] < '2023-10-16 00:00:00'
+[CLHOURS].[dbo].[CLHOURS]
+WHERE [ProdDate] > '2023-10-01 00:00:00' AND [ProdDate] < '2023-10-9 00:00:00'
 
 
 SELECT FORMAT(ROUND(SUM([Standard]) / 2040 * 100, 2), '0.##') AS [Standard]
 FROM 
-    [CLHOURS].[dbo].[CLHOURS]
-	WHERE [ProdDate] > '2023-10-15 00:00:00' AND [ProdDate] < '2023-10-23 00:00:00'
-
-	
+[CLHOURS].[dbo].[CLHOURS]
+WHERE [ProdDate] > '2023-10-08 00:00:00' AND [ProdDate] < '2023-10-16 00:00:00'
 
 
+SELECT FORMAT(ROUND(SUM([Standard]) / 2040 * 100, 2), '0.##') AS [Standard]
+FROM 
+[CLHOURS].[dbo].[CLHOURS]
+WHERE [ProdDate] > '2023-10-15 00:00:00' AND [ProdDate] < '2023-10-23 00:00:00'
 
+-----------------------------------
 
 DECLARE @StartDate DATETIME = '2023-09-24 00:00:00';
 DECLARE @EndDate DATETIME = '2024-04-16 00:00:00';
 
 SELECT 
-    DATEPART(week, [ProdDate]) AS semana,
-    FORMAT(ROUND(SUM([Standard]) / 2040 * 100, 2), '0.##') AS [Standard]
+DATEPART(week, [ProdDate]) AS semana,
+FORMAT(ROUND(SUM([Standard]) / 2040 * 100, 2), '0.##') AS [Standard]
 FROM 
-    [CLHOURS].[dbo].[CLHOURS]
+[CLHOURS].[dbo].[CLHOURS]
 WHERE 
-    [ProdDate] > @StartDate AND [ProdDate] < @EndDate
+[ProdDate] > @StartDate AND [ProdDate] < @EndDate
 GROUP BY 
-    DATEPART(week, [ProdDate])
+DATEPART(week, [ProdDate])
 ORDER BY 
-    MIN([ProdDate]);
+MIN([ProdDate]);
 
-
+-----------------------------------
 
 DECLARE @EndDate DATETIME = @thisDay
-                                DECLARE @StartDate DATETIME
+DECLARE @StartDate DATETIME
 
 
-                                SET @StartDate = DATEADD(WEEK, -31, @EndDate)
+SET @StartDate = DATEADD(WEEK, -31, @EndDate)
 
-                                CREATE TABLE #TempResultsDATAProyectallCLHOURS (
-                                    DateRangeStart DATETIME,
-                                    StandardPercentage VARCHAR(10)
-                                )
+CREATE TABLE #TempResultsDATAProyectallCLHOURS (
+DateRangeStart DATETIME,
+StandardPercentage VARCHAR(10)
+)
 
-                                WHILE @StartDate <= @EndDate
-                                BEGIN
-                                    INSERT INTO #TempResultsDATAProyectallCLHOURS (DateRangeStart, StandardPercentage)
-                                    SELECT @StartDate AS DateRangeStart, FORMAT(ROUND(SUM(Standard) / 2040 * 100, 2), '0.##') AS StandardPercentage
-                                    FROM CLHOURS
-                                    WHERE ProdDate >= DATEADD(DAY, 1, @StartDate) AND ProdDate < DATEADD(DAY, 8, @StartDate) AND Qty > 0
+WHILE @StartDate <= @EndDate
+BEGIN
+INSERT INTO #TempResultsDATAProyectallCLHOURS (DateRangeStart, StandardPercentage)
+SELECT @StartDate AS DateRangeStart, FORMAT(ROUND(SUM(Standard) / 2040 * 100, 2), '0.##') AS StandardPercentage
+FROM CLHOURS
+WHERE ProdDate >= DATEADD(DAY, 1, @StartDate) AND ProdDate < DATEADD(DAY, 8, @StartDate) AND Qty > 0
 
-                                    SET @StartDate = DATEADD(WEEK, 1, @StartDate)
-                                END
+SET @StartDate = DATEADD(WEEK, 1, @StartDate)
+END
 
-                                DELETE FROM #TempResultsDATAProyectallCLHOURS
-                                WHERE DateRangeStart = (SELECT MIN(DateRangeStart) FROM #TempResultsDATAProyectallCLHOURS)
+DELETE FROM #TempResultsDATAProyectallCLHOURS
+WHERE DateRangeStart = (SELECT MIN(DateRangeStart) FROM #TempResultsDATAProyectallCLHOURS)
 
-                                SELECT TOP(30)  * FROM #TempResultsDATAProyectallCLHOURS
-                                WHERE StandardPercentage IS NOT NULL
-                                ORDER BY DateRangeStart DESC
+SELECT TOP(30)  * FROM #TempResultsDATAProyectallCLHOURS
+WHERE StandardPercentage IS NOT NULL
+ORDER BY DateRangeStart DESC
 
-                                DROP TABLE #TempResultsDATAProyectallCLHOURS
- -----------------------------------
+DROP TABLE #TempResultsDATAProyectallCLHOURS
+-----------------------------------
 
 SELECT * FROM ( SELECT top 30 Week = DATEADD(DAY, - 1 * DATEPART(dw, ProdDate - 2 ), ProdDate ), Standard = round(SUM(Standard)/100,2) FROM CLHOURS WHERE  ProdDate <= DATEADD(mm, 8, GetDate()) AND ( WorkOrder IS NOT NULL AND PartNo IS NOT NULL AND Qty>0 AND Standard IS NOT NULL  ) GROUP BY DATEADD(DAY, - 1 * DATEPART(dw, ProdDate - 2 ),ProdDate ) ORDER BY WEEK DESC) a ORDER BY a.WEEK ASC
 
 ------------------------------------
 SELECT 
-                                        SUM(CASE WHEN STATUS = 'PENDING' AND ItemNumber NOT LIKE('%NRE%') AND ItemNumber NOT LIKE ('%M%') AND ItemNumber NOT LIKE ('%C%') AND ProductLine <> 'REP' THEN 1 ELSE 0 END) AS PENDING,
-                                        SUM(CASE WHEN STATUS LIKE '%PICK%' THEN 1 ELSE 0 END) AS PICK,
-                                        SUM(CASE WHEN STATUS LIKE '%STK%' THEN 1 ELSE 0 END) AS STOCK,
-                                        SUM(CASE WHEN STATUS LIKE '%PREP-LABEL%' THEN 1 ELSE 0 END) AS PREP_LABEL,
-                                        SUM(CASE WHEN STATUS LIKE '%PREP-WIRE%' THEN 1 ELSE 0 END) AS PREP_WIRE,
-                                        SUM(CASE WHEN STATUS LIKE '%WIP-READY%' THEN 1 ELSE 0 END) AS WIP_READY,
-                                        SUM(CASE WHEN STATUS LIKE '%WIP%' AND STATUS NOT LIKE '%-READY%' THEN 1 ELSE 0 END) AS WIP,
-                                        SUM(CASE WHEN STATUS LIKE '%TEST%' AND ItemNumber NOT LIKE('%NRE%') AND ItemNumber NOT LIKE ('%M%') AND ItemNumber NOT LIKE ('%C%') AND ProductLine <> 'REP' THEN 1 ELSE 0 END) AS TEST,
-                                        SUM(CASE WHEN STATUS LIKE '%INSP%' AND ItemNumber NOT LIKE('%NRE%') AND ProductLine <> 'REP' THEN 1 ELSE 0 END) AS INSPECTION,
-                                        SUM(CASE WHEN STATUS = 'PACKAGING' AND ItemNumber NOT LIKE('%NRE%') AND ItemNumber NOT LIKE ('%M%') AND ItemNumber NOT LIKE ('%C%') AND ProductLine <> 'REP' THEN 1 ELSE 0 END) AS PACKAGING,
-                                        SUM(CASE WHEN STATUS LIKE 'SHIP' AND ItemNumber NOT LIKE('%NRE%') AND ItemNumber NOT LIKE ('%M%') AND ItemNumber NOT LIKE ('%C%') AND ProductLine <> 'REP' THEN 1 ELSE 0 END) AS SHIPPING
-                                    FROM 
-                                        SO2_420
-                                    INNER JOIN 
-                                        WorkOrder_420 ON SO2_420.SalesOrderNo = WorkOrder_420.SalesOrderNumber 
-                                            AND SO2_420.LineKey = WorkOrder_420.LineIndex 
-                                    WHERE 
-                                        SO2_420.QuantityOrdered > SO2_420.QuantityShipped;
+SUM(CASE WHEN STATUS = 'PENDING' AND ItemNumber NOT LIKE('%NRE%') AND ItemNumber NOT LIKE ('%M%') AND ItemNumber NOT LIKE ('%C%') AND ProductLine <> 'REP' THEN 1 ELSE 0 END) AS PENDING,
+SUM(CASE WHEN STATUS LIKE '%PICK%' THEN 1 ELSE 0 END) AS PICK,
+SUM(CASE WHEN STATUS LIKE '%STK%' THEN 1 ELSE 0 END) AS STOCK,
+SUM(CASE WHEN STATUS LIKE '%PREP-LABEL%' THEN 1 ELSE 0 END) AS PREP_LABEL,
+SUM(CASE WHEN STATUS LIKE '%PREP-WIRE%' THEN 1 ELSE 0 END) AS PREP_WIRE,
+SUM(CASE WHEN STATUS LIKE '%WIP-READY%' THEN 1 ELSE 0 END) AS WIP_READY,
+SUM(CASE WHEN STATUS LIKE '%WIP%' AND STATUS NOT LIKE '%-READY%' THEN 1 ELSE 0 END) AS WIP,
+SUM(CASE WHEN STATUS LIKE '%TEST%' AND ItemNumber NOT LIKE('%NRE%') AND ItemNumber NOT LIKE ('%M%') AND ItemNumber NOT LIKE ('%C%') AND ProductLine <> 'REP' THEN 1 ELSE 0 END) AS TEST,
+SUM(CASE WHEN STATUS LIKE '%INSP%' AND ItemNumber NOT LIKE('%NRE%') AND ProductLine <> 'REP' THEN 1 ELSE 0 END) AS INSPECTION,
+SUM(CASE WHEN STATUS = 'PACKAGING' AND ItemNumber NOT LIKE('%NRE%') AND ItemNumber NOT LIKE ('%M%') AND ItemNumber NOT LIKE ('%C%') AND ProductLine <> 'REP' THEN 1 ELSE 0 END) AS PACKAGING,
+SUM(CASE WHEN STATUS LIKE 'SHIP' AND ItemNumber NOT LIKE('%NRE%') AND ItemNumber NOT LIKE ('%M%') AND ItemNumber NOT LIKE ('%C%') AND ProductLine <> 'REP' THEN 1 ELSE 0 END) AS SHIPPING
+FROM 
+SO2_420
+INNER JOIN 
+WorkOrder_420 ON SO2_420.SalesOrderNo = WorkOrder_420.SalesOrderNumber 
+AND SO2_420.LineKey = WorkOrder_420.LineIndex 
+WHERE 
+SO2_420.QuantityOrdered > SO2_420.QuantityShipped;
+
 ---------------------------------------------------------
 
 SELECT 
-    MIN(DueDate) AS EarliestDueDate, 
-    FORMAT(SUM(QtyDue * Cost), '0') AS TotalCost
+MIN(DueDate) AS EarliestDueDate, 
+FORMAT(SUM(QtyDue * Cost), '0') AS TotalCost
 FROM 
-    PO
+PO
 GROUP BY 
-    DATEPART(YEAR, DueDate), 
-    DATEPART(WEEK, DueDate)
+DATEPART(YEAR, DueDate), 
+DATEPART(WEEK, DueDate)
 ORDER BY 
-    EarliestDueDate ASC;
+EarliestDueDate ASC;
